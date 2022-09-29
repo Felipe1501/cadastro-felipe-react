1062da1c5f4aa49b0dfbb928f38867f044dc8f17
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-29 09:36:18 -0300

    init project

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Projeto usando componentes do React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Cadastro em React!</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por 
+            <strong> <span className="text-danger">Felipe</span></strong>
+        </span>
+    </footer>
